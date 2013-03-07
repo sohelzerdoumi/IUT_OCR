3c543691196098cc6da880f9ef91e967d1673a60
Zerdoumi Sohel <sohel.zerdoumi@gmail.com>
2013-03-07 08:26:15 +0100

    integration Caracteristique

--- include/MyImage.h
+++ include/MyImage.h
@@ -3,29 +3,28 @@
 #include "CImg.h"
 
 #include <string>
-#include "Histogramme.h"
-#define ZONING_LARGEUR 3
-#define ZONING_HAUTEUR 3
-#define ZONING_DIFF_MAX 100
+#include <vector>
+#include "caracteristique/Caracteristique.h"
+
 
 
 class MyImage
 {
     public:
         MyImage(const MyImage & i);
+        MyImage(const MyImage & i,int largeur, int hauteur);
         MyImage(std::string filename);
         virtual ~MyImage();
 
-        void generateHistogrammes();
         float compare(const MyImage & i);
-        float compareZoning(const MyImage & i);
-        void    generateZoning();
         void display();
     private:
-        double                          _zone[ZONING_LARGEUR][ZONING_HAUTEUR];
+        std::vector<Caracteristique *>  _caracteristiques;
+        void                        generateCaracteristiques();
+        void                        loadCaracteristiques();
         cimg_library::CImg<int>        _cimg;
-        Histogramme hHorizontal;
-        Histogramme hVertical;
+//        Histogramme hHorizontal;
+//        Histogramme hVertical;
         std::string                 _filename;
         void                        toBinary();
 };
--- src/MyImage.cpp
+++ src/MyImage.cpp
@@ -7,25 +7,31 @@ using namespace cimg_library;
 using namespace std;
 
 MyImage::MyImage(std::string filename)
-: _cimg(filename.c_str()), hHorizontal(&_cimg, HISTOGRAMME_HORIZONTAL),hVertical(&_cimg, HISTOGRAMME_VERTICAL)
+: _cimg(filename.c_str())
 {
     _filename = filename;
     toBinary();
-    //_cimg.erode(1,1);
-    generateZoning();
-    generateHistogrammes();
-    //cout << _cimg.variance()<< endl;;
-    //_cimg.display();
-
+    _cimg.autocrop(255);
+    loadCaracteristiques();
 }
 
-MyImage::MyImage(const MyImage & i)
-: _cimg( i._filename.c_str()), hHorizontal(&_cimg, HISTOGRAMME_HORIZONTAL),hVertical(&_cimg, HISTOGRAMME_VERTICAL)
+MyImage::MyImage(const MyImage & i, int largeur, int hauteur)
+: _cimg(i._filename.c_str())
 {
     _filename = i._filename;
     toBinary();
-    generateZoning();
-    generateHistogrammes();
+    _cimg.autocrop(255);
+    _cimg.resize(largeur,hauteur);
+    loadCaracteristiques();
+}
+
+MyImage::MyImage(const MyImage & i)
+: _cimg( i._filename.c_str())//, hHorizontal(&_cimg, HISTOGRAMME_HORIZONTAL),hVertical(&_cimg, HISTOGRAMME_VERTICAL)
+{
+    _filename = i._filename;
+    toBinary();
+    _cimg.autocrop(255);
+    loadCaracteristiques();
 }
 
 void MyImage::toBinary(){
@@ -37,7 +43,7 @@ void MyImage::toBinary(){
     for(int c = 0; c < largeur ; c++ )
         for(int l = 0 ; l < hauteur ; l++){
             currentPixel = _cimg.data(c,l);
-            currentPixel[0] = (currentPixel[0] > SEUIL) * 255;
+            currentPixel[0] = (currentPixel[0] > 200) * 255;
         }
 
 }
@@ -46,39 +52,26 @@ void MyImage::toBinary(){
 
 
 float MyImage::compare(const MyImage & i){
-    MyImage tmp(i);
-    tmp._cimg.resize( _cimg.width() , _cimg.height() );
-    tmp.toBinary();
-    tmp.generateZoning();
-    tmp.generateHistogrammes();
+    MyImage tmp(i,_cimg.width() , _cimg.height());
+    int diff = 0; //( hHorizontal.compare(tmp.hHorizontal ) + hVertical.compare(tmp.hVertical ) );
+    for(int i=0; i < (signed)_caracteristiques.size() ; i++)
+        diff += _caracteristiques[i]->compare(tmp._caracteristiques[i]);
 
-    int diffHistogramme = ( hHorizontal.compare(tmp.hHorizontal ) + hVertical.compare(tmp.hVertical ) );
-
-
-    return diffHistogramme;//*compareZoning(i);//*(isoDiff == 0 ? 1 : 1+isoDiff/50) ;
+    return diff;//*compareZoning(i);//*(isoDiff == 0 ? 1 : 1+isoDiff/50) ;
 }
 
-float MyImage::compareZoning(const MyImage & i){
-    float diff = 0;
-      for(int c = 0; c < ZONING_LARGEUR; c++)
-        for(int l =0 ; l <ZONING_HAUTEUR ; l++)
-            diff += abs(_zone[c][l] - i._zone[c][l]);
-    //cout << diff << endl;
-    if( diff > (ZONING_LARGEUR*ZONING_HAUTEUR)*ZONING_DIFF_MAX )
-        diff= 100000;
-    return diff;
+void MyImage::generateCaracteristiques(){
+    for(int i=0; i < (signed)_caracteristiques.size() ; i++)
+        _caracteristiques[i]->generate();
+
 }
+void MyImage::loadCaracteristiques(){
+    //_caracteristiques.push_back( new  Histogramme(&_cimg, HISTOGRAMME_HORIZONTAL));
+    //_caracteristiques.push_back( new  Histogramme(&_cimg, HISTOGRAMME_VERTICAL));
+    _caracteristiques.push_back( new  Zoning(&_cimg));
 
 
-void MyImage::generateZoning(){
-    int largeur = _cimg.width()/ZONING_LARGEUR-1;
-    int hauteur = _cimg.height()/ZONING_HAUTEUR-1;
-    for(int c = 0; c < ZONING_LARGEUR; c++)
-        for(int l =0 ; l < ZONING_HAUTEUR ; l++)
-            _zone[c][l] = _cimg.get_crop( c*largeur, l*hauteur ,0,
-                                         (c+1)*largeur , (l+1)*hauteur,0 ).mean();
-
-
+    generateCaracteristiques();
 }
 
 void MyImage::display(){
@@ -86,11 +79,8 @@ void MyImage::display(){
 
 }
 
-MyImage::~MyImage(){}
+MyImage::~MyImage(){
+    for(int i=0; i < (signed)_caracteristiques.size() ; i++)
+        delete _caracteristiques[i];
 
-
-void MyImage::generateHistogrammes(){
-    _cimg.autocrop(255);
-    hHorizontal.generate();
-    hVertical.generate();
 }
